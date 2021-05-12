490de2ec7c82a8dff17d41047f94541678386585
marorcua <spulcrum@gmail.com>
2021-05-12 19:24:26 +0200

    admin dashboard beginning

--- client/src/components/pages/admin/Dashboard.js
+++ client/src/components/pages/admin/Dashboard.js
@@ -1,22 +1,47 @@
 import { Component } from "react"
 import { Container } from "react-bootstrap"
 import { Redirect } from "react-router-dom/cjs/react-router-dom.min"
-
+import LineChart from "./LineChart"
+import testData from './data'
+import AdminService from "../../../service/admin.service"
 
 class Dashboard extends Component {
     constructor() {
         super()
         this.state = {
-
+            data: undefined
         }
+        this.adminService = new AdminService()
     }
 
+    componentDidMount() {
+        this.adminService
+            .getOrdersData()
+            .then(response => {
+                console.log("the response from server:", response.data)
+                this.setState({ data: response.data })
+            })
+            .catch(err => console.log(err))
+    }
+
+    getData(data) {
+        console.log(data)
+    }
+
+    getDataClients() {
+
+    }
+
+
     render() {
+        //console.log(this.state.data)
+        //console.log(testData)
         return (!this.props.loggedUser || this.props.loggedUser.role !== 'ADMIN') ? <Redirect to="/" /> :
             (
-                <Container>
+                <Container style={{ height: "300px" }}>
                     <h1>Dashboard</h1>
-                </Container>)
+                    <LineChart data={this.state.data ? this.state.data : testData} />
+                </Container >)
     }
 }
 
--- client/src/components/routes/Routes.js
+++ client/src/components/routes/Routes.js
@@ -25,12 +25,12 @@ const Routes = ({ storeUser, loggedUser, handleAlert }) => {
             <Route path="/product/:id" render={props => <ProductDetails {...props} />} />
             <Route path="/product/brand/:supplier_id" render={props => <SupplierPage {...props} />} />
 
-            <Route path="/admin" exact render={() => <SupplierList loggedUser={loggedUser} />} />
-            <Route path="/admin/clients" render={() => <ClientList />} />
-            <Route path="/admin/suppliers" render={() => <SupplierList />} />
+            <Route path="/admin" exact render={() => <Dashboard loggedUser={loggedUser} />} />
+            <Route path="/admin/clients" render={() => <ClientList loggedUser={loggedUser} />} />
+            <Route path="/admin/suppliers" render={() => <SupplierList loggedUser={loggedUser} />} />
 
-            <Route path="/supplier/myarea/:id" exact render={props => <SupplierProfile loggedUser={loggedUser} {...props}/>} />
-            <Route path="/suppliers/signup" render={props => <SignupForm history={props.history} />} />
+            <Route path="/supplier/myarea/:id" exact render={props => <SupplierProfile loggedUser={loggedUser} {...props} />} />
+            <Route path="/supplier/signup" render={props => <SignupForm history={props.history} />} />
             <Route path="/supplier/productdetails" render={() => <MyProductCard />} />
             <Route path="/supplier/options" render={() => <NewOption />} />
 
