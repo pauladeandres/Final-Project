58ea1df67d35e26e542198461f5e03c0b5ed90a5
marorcua <spulcrum@gmail.com>
2021-05-21 12:29:40 +0200

    minor fixes

--- client/src/components/pages/Admin/ClientCard.js
+++ client/src/components/pages/Admin/ClientCard.js
@@ -13,7 +13,7 @@ const ClientCard = ({ number, edit, editRole, loadClients, loggedUser, ...props 
 
                 <td> {(props.role === 'SUPPLIER' && props.client) ? <Link to={`/supplier/myarea/${userId}`}> {props.email}</Link> : props.email} </td>
 
-                <td>{(props.role === 'CUSTOMER' && props.client.order.length) ? props.client.order.length : (props.client ? props.client.products.length : null)}</td>
+                <td>{(props.role === 'CUSTOMER' && props.client && props.client.order.length) ? props.client.order.length : (props.client ? props.client.products.length : null)}</td>
 
                 <td>{props.favoriteProducts ? props.favoriteProducts.length : null}</td>
                 {props.client ? (props.role !== 'SUPPLIER' ? <td onClick={(e) => edit(e, props.client)}>Edit</td> : <td><Link to={`/supplier/myarea/${userId}`}> Profile</Link></td>) : <td></td>}
